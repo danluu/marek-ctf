c0328634583e4feef769a20620d51ba32127405d
Dan Luu <danluu@gmail.com>
2013-03-13 17:17:21 -0400

    update to python 3

--- ex3.py
+++ ex3.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 
 import time
 import random
 import string
 
-print int(time.time() * 256)
-print int(time.time()) - 123170
+print (int(time.time() * 256))
+print (int(time.time()) - 123170)
 
 random.seed(348946227264)
 #tried this: for m in range(348946227264, 348946227294): ,didn't *256
@@ -14,5 +14,5 @@ random.seed(348946227264)
 for m in range(1363071202, 1363071232):
   random.seed(m*256)
   secret = ''.join(random.choice(string.ascii_letters) for i in range(12))
-  print secret
+  print (secret)
 
