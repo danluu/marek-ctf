90f63f75e931a79068d8ecad498a4b473cc940f1
Dan Luu <danluu@gmail.com>
2013-03-13 16:41:17 -0400

    Kill newline from passwords

--- ex0.rb
+++ ex0.rb
@@ -5,6 +5,8 @@ require 'net/http'
 uri = URI('http://pragmaticcrypto.herokuapp.com/exercise0/')
 dict = IO.readlines('john.txt')
 dict.each{|p|
-  response = Net::HTTP.post_form(uri, 'password' => p)
+#  response = Net::HTTP.post_form(uri, 'password' => p)
+  response = Net::HTTP.post_form(uri, 'password' => p.chomp)
+  puts p
   puts response
 }
