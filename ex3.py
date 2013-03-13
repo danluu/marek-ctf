6bcea9190419ba6b0d0a1d27401c036e7aa16590
Dan Luu <danluu@gmail.com>
2013-03-13 18:16:10 -0400

    correctly get problem 3

--- ex3.py
+++ ex3.py
@@ -1,18 +1,34 @@
-#!/usr/local/bin/python3
+#!/Users/danluu/Dropbox/classes/HS/marek-crypto/john-1.7.9/doc/test2/bin/python3
 
 import time
 import random
 import string
+import re
+import requests
 
-print (int(time.time() * 256))
-print (int(time.time()) - 123170)
 
-random.seed(348946227264)
-#tried this: for m in range(348946227264, 348946227294): ,didn't *256
-#tried this: for m in range(1363071202, 1363071232):, did *256
+import urllib.request
+f = urllib.request.urlopen("http://pragmaticcrypto.herokuapp.com/exercise3/")
+site = f.read()
+marek_delay = int(re.search(b'(\d+) seconds', site).groups()[0]) + 1
+mytime = int(time.time())
 
-for m in range(1363071202, 1363071232):
-  random.seed(m*256)
-  secret = ''.join(random.choice(string.ascii_letters) for i in range(12))
-  print (secret)
+time_guess = mytime - marek_delay
+
+url = r'http://pragmaticcrypto.herokuapp.com/exercise3/'
+a = 2348390
+
+s = requests.Session()
+
+for skew in range(-1, 2):
+  for m in range(0, 256):
+    random.seed((time_guess + skew) * 256 + m)
+    secret = ''.join(random.choice(string.ascii_letters) for i in range(12))
+    res = s.post(url, data={'password': secret})
+    if res.status_code == 200:
+      print(secret)
+      print(res)
+      break
+
+
 
